9d067273843b40e4bb4217240b81203754feaa82
Siddharaj Jawalkar <siddharajjawalkar25@gmail.com>
2020-04-22 13:20:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/zoo/AnimalInfo.kt
@@ -0,0 +1,23 @@
+package com.example.zoo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_animal_info.*
+
+class AnimalInfo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animal_info)
+
+        val bundle:Bundle = intent.extras!!
+        val name = bundle.getString("name")
+        val des = bundle.getString("des")
+        val image = bundle.getInt("image")
+
+        ivAnimaIimage.setImageResource(image)
+        tvName.text = name
+        tvDes.text = des
+
+    }
+}
